4ce75cf84ff54254f11daa75f8653ccb492ace55
Alexsandro Silva Alves <alexsandro_alves@ecglobalsolutions.com>
2017-08-11 14:39:41 -0300

    Auth with Google and Facebook

---
+++ src/components/Terms.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <section>
+    <header>
+      <h1>Terms</h1>
+    </header>
+    <div class="">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam turpis nec nisl interdum, tempus sagittis sem condimentum. Curabitur gravida pharetra ex, vitae hendrerit tortor auctor nec. Donec odio tortor, imperdiet vel accumsan sit amet, consectetur sed dui. Phasellus quis tortor posuere, condimentum est at, ullamcorper diam. Maecenas lobortis luctus risus, a rutrum ex porta feugiat. Ut dignissim diam ac turpis tempor, nec dignissim lorem consequat. Nunc in rhoncus massa, vitae dapibus lectus. Sed eu aliquet odio. Curabitur eget pulvinar sapien. Praesent mattis libero ac sem semper, at tempor sem maximus. In id ullamcorper elit, vitae mattis justo. Integer vel tincidunt turpis, eu porttitor sem. Integer fringilla nec odio nec venenatis. Proin egestas nulla ligula, vitae ultricies felis fermentum eget. Morbi fermentum pretium felis tristique dictum. Ut pretium dui ac rhoncus fringilla.</p>
+      <p>Integer egestas turpis ipsum, placerat vulputate ipsum sagittis non. Cras vitae tristique risus, quis finibus ex. Sed cursus rhoncus erat, in laoreet velit sodales at. Donec maximus porta nisl tincidunt molestie. In in nibh in lorem eleifend mattis cursus ut metus. Sed sapien augue, mattis quis facilisis eget, fringilla ac ex. Sed efficitur tortor quis vulputate dapibus. Maecenas mattis quam orci, in gravida libero luctus non.</p>
+      <p>Aenean luctus molestie lacus nec tincidunt. Vestibulum sit amet vulputate lacus. Curabitur posuere rutrum purus non fermentum. Nullam nec nisi at eros varius vehicula ac eu lectus. Vivamus vehicula libero eros, ac sagittis risus pharetra sit amet. Mauris nec venenatis eros. Vivamus facilisis laoreet lorem, in lobortis ex lobortis ornare. Cras quis ante vitae felis dignissim varius. Donec tincidunt mauris et ligula posuere, ut blandit libero vulputate. Nunc nisl lacus, eleifend ut elit et, cursus semper est. Sed id pretium leo, ac congue neque. Nullam rhoncus rutrum augue, ac gravida dolor rutrum id. Etiam euismod libero id luctus pharetra. Integer eget vulputate leo. Vivamus finibus nulla sit amet dictum finibus.</p>
+      <p>Fusce ut dolor at orci pulvinar placerat nec sed sapien. Sed pretium convallis tempus. Quisque tincidunt condimentum neque, vitae semper est blandit quis. Proin tincidunt ac tortor quis porta. Curabitur dictum ligula sit amet vulputate ornare. Ut id lectus ultricies elit tristique gravida vitae a turpis. Nam bibendum metus ut sapien interdum, at vestibulum nisl tincidunt. Proin venenatis, turpis quis posuere iaculis, metus diam dignissim dui, eget ultricies metus libero a enim. Proin vitae semper dui, in auctor magna.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'terms'
+}
+</script>
+
+<style lang="css">
+</style>
