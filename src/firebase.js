af848a096528e4c4701d94998559827447bd1e72
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2017-08-14 17:56:43 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 export const config = {
-  apiKey: 'AIzaSyDLWBGWadoyHqT2ac0NQD9DchmyQwyx0Q4',
-  authDomain: 'check-point-9c07a.firebaseapp.com',
-  databaseURL: 'https://check-point-9c07a.firebaseio.com',
-  projectId: 'check-point-9c07a',
-  storageBucket: 'check-point-9c07a.appspot.com',
-  messagingSenderId: '657116448929'
+  apiKey: '...',
+  authDomain: '...',
+  databaseURL: '...',
+  projectId: '...',
+  storageBucket: '...',
+  messagingSenderId: '...'
 }
